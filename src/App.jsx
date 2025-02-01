2370623cfa447dc5ad4208acbfbd4370f4940a52
rezuan98 <rezuanahmmeds@gmail.com>
2025-02-01 12:12:36 +0600

    added a text

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
+
+
 
 function App() {
 
   return (
     <>
         <h4 className="bg-green-900">Hello React</h4>
+       <span> <h1 className='text-center p-2 bg-amber-800 text-white'>My Name is Rezuan</h1></span>
+      
     </>
   )
 }
