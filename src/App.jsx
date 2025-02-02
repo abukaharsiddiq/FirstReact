a465c2e435230aca3e8607b7d0d8a3a808ccff38
rezuan98 <rezuanahmmeds@gmail.com>
2025-02-02 18:55:07 +0600

    slider added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
-
+import ResponsiveSlider from "./components/ResponsiveSlider";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <ResponsiveSlider />
       <Footer />
     </>
   );
