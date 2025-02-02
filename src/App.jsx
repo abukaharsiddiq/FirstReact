85ea6874e23da25e082d4b9b9237fa22d0d9ba1e
rezuan98 <rezuanahmmeds@gmail.com>
2025-02-02 18:33:50 +0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
-import { useState } from 'react';
 import Header from "./components/Header";
-=======
-
-
->>>>>>> branch-rezuan
 
 function App() {
-
   return (
     <>
-<<<<<<< HEAD
-        <Header/>
-=======
-        <h4 className="bg-green-900">Hello React</h4>
-       <span> <h1 className='text-center p-2 bg-amber-800 text-white'>My Name is Rezuan</h1></span>
-      
->>>>>>> branch-rezuan
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
