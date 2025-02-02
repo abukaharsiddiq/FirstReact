f8873fab9999837769ef7fd78f82ea31614920bd
abukaharsiddiq <abukaharsiddiq2018@gmail.com>
2025-02-02 19:18:22 +0600

    all

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
 import Header from "./components/Header";
-<<<<<<< HEAD
 import About from "./components/About";
 import Services from "./components/Services";
 import ContactSection from "./components/ContactSection";
-=======
-
->>>>>>> c6386d1971ad38212e4062c052c6aa2e24cc1809
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
         <Header/>
         <About/>
         <Services/>
         <ContactSection/>
         <Footer/>
-=======
-      <Header />
-      <Footer />
->>>>>>> c6386d1971ad38212e4062c052c6aa2e24cc1809
     </>
   );
 }
